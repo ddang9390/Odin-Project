17c27fe9a7a4291d27a78fea71c4cbe13451a43e
ddang9390 <dnld1ng@yahoo.com>
2024-09-12 14:37:59 -0400

    Initial commit for calculator project

---
+++ Calculator/calculator.js
@@ -0,0 +1,19 @@
+let operator = ""
+let num1 = 0
+let num2 = 0
+
+function add(num1, num2){
+    return num1 + num2;
+}
+
+function subtract(num1, num2){
+    return num1 - num2;
+}
+
+function multiply(num1, num2){
+    return num1 * num2;
+}
+
+function divide(num1, num2){
+    return num1 * num2
+}
