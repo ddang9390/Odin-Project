9e0e5d2950257811d0caeeee7e024366c54b681e
ddang9390 <dnld1ng@yahoo.com>
2024-09-12 14:42:37 -0400

    Simple layout of calculator

--- Calculator/calculator.js
+++ Calculator/calculator.js
@@ -17,3 +17,18 @@ function multiply(num1, num2){
 function divide(num1, num2){
     return num1 * num2
 }
+
+function operate(operator, num1, num2){
+    switch(operator){
+        case "add":
+            add(num1, num2);
+        case "subtract":
+            subtract(num1, num2);
+        case "multiply":
+            multiply(num1, num2);
+        case "divide":
+            divide(num1, num2);
+        default:
+            alert("Unexpected error")
+    }
+}
