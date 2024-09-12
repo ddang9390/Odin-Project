5db0e7139a3aee4977eade0c4fc2cc63399be9f2
ddang9390 <dnld1ng@yahoo.com>
2024-09-12 16:10:01 -0400

    Able to input numbers into input field through number buttons

--- Calculator/calculator.js
+++ Calculator/calculator.js
@@ -1,6 +1,6 @@
-let operator = ""
-let num1 = 0
-let num2 = 0
+let operator = "";
+let num1 = undefined;
+let num2 = undefined;
 
 function add(num1, num2){
     return num1 + num2;
@@ -19,16 +19,40 @@ function divide(num1, num2){
 }
 
 function operate(operator, num1, num2){
+    let answer = undefined;
     switch(operator){
         case "add":
-            add(num1, num2);
+            answer = add(num1, num2);
         case "subtract":
-            subtract(num1, num2);
+            answer = subtract(num1, num2);
         case "multiply":
-            multiply(num1, num2);
+            answer = multiply(num1, num2);
         case "divide":
-            divide(num1, num2);
+            answer = divide(num1, num2);
         default:
             alert("Unexpected error")
     }
+
+    console.log(answer)
+}
+
+function equals(){
+    if(num1 != undefined && num2 != undefined && operator != ""){
+        operate(operator, num1, num2)
+    }
+}
+
+function storeNumber(o, num){
+    operator = o;
+    if(num1 == undefined){
+        num1 = num;
+    }
+    else{
+        num2 = num;
+    }
+}
+
+function inputNumber(num){
+    let inputField = document.querySelector("#inputField");
+    inputField.value += num
 }
